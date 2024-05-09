d9677f8b745dcd63f45fa4a031ca494096c5554c
prince patel <141562215+prince2004patel@users.noreply.github.com>
2024-05-09 08:09:04 +0530

    super_keyword.java

--- super_keyword.java
+++ super_keyword.java
@@ -1,3 +1,8 @@
+//The super keyword refers to superclass (parent) objects. It is used to call superclass methods,
+//and to access the superclass constructor. The most common use of the super keyword is to eliminate 
+//the confusion between superclasses and subclasses that have methods with the same name.
+
+
 class Animal2{
     public void animalsound(){
         System.out.println("the animal make diff. sound");
